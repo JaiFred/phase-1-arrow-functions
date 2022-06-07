c27fff3691d0bbaac4a7069f66c0ffeffb1a553f
JaiVon <frederickjaivon@yahoo.com>
2022-06-07 19:23:33 -0400

    commit tree fe50ea531984cb33110e0e53e99d26f9ecb0af32

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (a,b) => {
+    console.log(`dividing ${a}`);
+    console.log(`dividing ${b}`);
+    return a / b;
+}
+divide(128,8);
+
+const square = (x) => x * x;
+
+const add = (a,b) => {
+    console.log(`adding ${a}`);
+    console.log(`adding ${b}`);
+    return a + b;
+}
+add(a,b);
